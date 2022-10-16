f91f64df09bcf4207bbd00deabcd485a1e9909b5
Cole <israsanfuen@gmail.com>
2022-10-16 22:52:15 +0200

    Ejercicio 6 Terminado

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int n;
+    float x,resul=1;
+    printf("Introduce la base: ");
+    scanf("%f",&x);
+    printf("Introduce el exponente: ");
+    scanf("%d",&n);
+    if (n!=0){
+        for (int i = n; i > 0; i--)
+            resul*=x;
+        if (n<0){
+            for (int i = -n; i > 0; i--)
+                resul*=x;
+            resul=1/resul;
+        }
+    }
+    printf("%f elevado a %d es: %f",x,n,resul);
     return 0;
 }
